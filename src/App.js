eba8868e39a5f2677f0ef82c7b91ec0f405b5903
Rob-Martinez <86384534+Rob-Martinez@users.noreply.github.com>
2022-06-28 09:11:45 -0500

    last push for now

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+
   render () {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm />
-      <FaceRecognition /> */}
+      <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}/>
+      {/* <FaceRecognition /> */}
     </div>
   );
   }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => {
     return (
         <div className="ma4 mt0">
-
+            {/* no logo here, not using this. But will keep in case of need in later use. */}
         </div>
     );
 }
